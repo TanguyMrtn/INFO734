460f542c605cee6073accfb246285b6040915fd9
TanguyMrtn <tanguy.mariton38@gmail.com>
2019-09-20 11:45:26 +0200

    todos added

--- firstTest/todo/controllers/index.js
+++ firstTest/todo/controllers/index.js
@@ -1,7 +1,34 @@
-function getAllTodos(req, res) {
+//function getAllTodos(req, res) {
   
-  res.render('fiche',{Title:'Info734'});
+//  res.render('fiche',{Title:'ROSA IS THE WEIRDEST RANDOM EVER'});
+
+//}
+
+function getAllTodos(req, res) {
+  const Todo = require('../../todo/models');
+
+  Todo.find({}, function(err, todos) {
+
+    if (err) throw err;
+
+    res.render('todos',{todos:todos})
+
+  });
+
+}
+
+function getOneTodo(req, res) {
+  const Todo = require('../../todo/models');
+
+  Todo.find({_id : req.params.id}, function(err, todo) {
+
+    if (err) throw err;
+
+    res.render('todo',{todo:todo});
+
+  });
 
 }
 
 module.exports.getAllTodos = getAllTodos;
+module.exports.getOneTodo = getOneTodo;
