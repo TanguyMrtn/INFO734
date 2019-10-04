9bce42857b466ed4c7cc9b4d0f1c5f142bfde6a7
TanguyMrtn <tanguy.mariton38@gmail.com>
2019-10-04 21:07:21 +0200

    ionic files added

---
+++ firstTest/app/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'add', loadChildren: './add/add.module#AddPageModule' },
+  { path: 'view', loadChildren: './view/view.module#ViewPageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
